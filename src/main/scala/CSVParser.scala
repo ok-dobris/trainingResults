76aefe1afb7e8eb38827fb73514ee0caa8928ff6
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-04-09 15:05:04 +0200

    Simplify CSV parser.

--- src/main/scala/CSVParser.scala
+++ src/main/scala/CSVParser.scala
@@ -5,7 +5,8 @@ object CSVParser extends RegexParsers{
   def plainString = "[^,]*".r ^^ identity
   def stringInQuotes = "\"" ~ "[^\"]*".r ~ "\"" ^^ {case _ ~ qstr ~ _ => qstr}
   def string = stringInQuotes | plainString
-  def lastItem: Parser[List[String]] = string ^^ {str => List(str)}
-  def moreItems: Parser[List[String]] = string ~ "," ~ line ^^ {case head ~ _ ~ tail => head :: tail}
-  def line = moreItems | lastItem
+  def line: Parser[List[String]] = (
+    string ~ "," ~ line ^^ {case head ~ _ ~ tail => head :: tail}
+    | string ^^ {str => List(str)}
+  )
 }
