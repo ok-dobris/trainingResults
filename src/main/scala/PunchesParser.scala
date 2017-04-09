0802669cb7dc0b86b8505f4fc7c618d73693ffe8
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-04-09 14:50:57 +0200

    CSV and punch parse working.

--- src/main/scala/PunchesParser.scala
+++ src/main/scala/PunchesParser.scala
@@ -1,5 +1,13 @@
 import scala.util.parsing.combinator._
 
 object PunchesParser extends JavaTokenParsers {
-  def punches: Parser[List[(Int, Int)]] = ???
+  def number: Parser[Int] =  decimalNumber ^^ {_.toInt}
+
+  def singlePunch: Parser[(Int, Int, Int)] = "[" ~ number ~ "," ~ number ~ "," ~ number ~ "]" ^^ {case _ ~ n1 ~ _ ~  n2 ~ _ ~ n3 ~ _ => (n1, n2, n3)}
+
+  def punchSeq: Parser[List[(Int, Int, Int)]] =
+      singlePunch ~ "," ~ punchSeq ^^ {case head ~ _ ~ tail => head :: tail} |
+        singlePunch ^^ {head => List(head)}
+
+  def punches: Parser[List[(Int, Int, Int)]] = "[" ~> punchSeq <~ "]"
 }
