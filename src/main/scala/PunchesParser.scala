7fda61cdb4820ca1595819269544b4efcf3e78da
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-05-30 22:50:10 +0200

    Fix: graceful handling of no punch, only Start - Finish.

--- src/main/scala/PunchesParser.scala
+++ src/main/scala/PunchesParser.scala
@@ -3,11 +3,12 @@ import scala.util.parsing.combinator._
 object PunchesParser extends JavaTokenParsers {
   def number: Parser[Int] =  decimalNumber ^^ {_.toInt}
 
+  def noPunch = "[" ~ "]" ^^ {_ => List[(Int, Int, Int)]()}
   def singlePunch: Parser[(Int, Int, Int)] = "[" ~ number ~ "," ~ number ~ "," ~ number ~ "]" ^^ {case _ ~ n1 ~ _ ~  n2 ~ _ ~ n3 ~ _ => (n1, n2, n3)}
 
   def punchSeq: Parser[List[(Int, Int, Int)]] =
       singlePunch ~ "," ~ punchSeq ^^ {case head ~ _ ~ tail => head :: tail} |
         singlePunch ^^ {head => List(head)}
 
-  def punches: Parser[List[(Int, Int, Int)]] = "[" ~> punchSeq <~ "]"
+  def punches: Parser[List[(Int, Int, Int)]] = "[" ~> punchSeq <~ "]" | noPunch
 }
